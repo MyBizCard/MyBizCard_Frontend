614a7d87fd64ce13ff2468b134467796414ca5ee
Jenny Gao <jennygao1023@gmail.com>
2018-06-14 22:43:47 -0400

    created registration form

--- MyBizCard/App.js
+++ MyBizCard/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StackNavigatior } from 'react-navigation'
+import RegForm from './components/RegForm';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+      <Image 
+        source={{uri: "https://cdn.iconscout.com/public/images/icon/premium/png-512/business-card-id-identification-3e87bf4e0dc8e389-512x512.png"}}
+        style={{width: 100, height: 100}} 
+      />
+        <RegForm />
         <Text style={styles.baseText}>
-        <Image 
-          source={{uri: "https://cdn.iconscout.com/public/images/icon/premium/png-512/business-card-id-identification-3e87bf4e0dc8e389-512x512.png"}}
-          style={{width: 100, height: 100}} 
-        />
-        <Text style={styles.titleText}>Sharing your business card just got a lot easier!</Text>
+        {/* <Text style={styles.titleText}>Sharing your business card just got a lot easier!</Text> */}
         </Text>
       </View>
     );
@@ -20,9 +23,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#36485f',
+    // alignItems: 'center',
     justifyContent: 'center',
+    paddingLeft: 50,
+    paddingRight: 50,
   },
   baseText: {
     fontFamily: 'Cochin',
---
+++ MyBizCard/components/RegForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.regform}>
+        <Text style={styles.header}>Registration</Text>
+        <TextInput style={styles.textinput} placeholder="Your name" underlineColorAndroid={'transparent'} />
+        <TextInput style={styles.textinput} placeholder="Your email" underlineColorAndroid={'transparent'} />
+        <TextInput style={styles.textinput} placeholder="Your password" underlineColorAndroid={'transparent'} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.btntext}>Sign up</Text>
+        </TouchableOpacity> 
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  regform: {
+    alignSelf: 'stretch'
+  },
+  header: {
+    fontSize: 24,
+    color: '#fff',
+    paddingBottom: 10,
+    marginBottom: 40,
+    borderBottomColor: '#199187',
+    borderBottomWidth: 1
+  },
+  textinput: {
+    alignSelf: 'stretch',
+    height: 40,
+    marginBottom: 30,
+    color: '#fff',
+    borderBottomColor: '#f8f8f8',
+    borderBottomWidth: 1
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#59cbbd',
+    marginTop: 30
+  },
+  btntext: {
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
